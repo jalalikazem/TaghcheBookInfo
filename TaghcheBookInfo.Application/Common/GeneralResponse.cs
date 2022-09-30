64a67bda31e9dfacaaedb48d53259cd560e4b042
kaafj <kaafj@DESKTOP-LL89R6R>
2022-09-30 18:33:30 +0330

    remove unnecessary file

--- TaghcheBookInfo.Application/Common/GeneralResponse.cs
+++ TaghcheBookInfo.Application/Common/GeneralResponse.cs
@@ -3,23 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static TaghcheBookInfo.Application.Common.Enums;
+
 
 namespace TaghcheBookInfo.Application.Common
 {
     public class GeneralResponse
     {
-        public GeneralResponse(bool succeeded, GeneralErrors error = GeneralErrors.NoError)
+        public GeneralResponse(bool succeeded)
         {
             Succeeded = succeeded;
-            Error = error;
         }
 
         public bool Succeeded { get; }
-        public GeneralErrors Error { get; }
         public string Message { get; set; }
         public object Data { get; set; }
-        public GeneralResponse(bool succeeded, object data, string message, GeneralErrors error = GeneralErrors.NoError)
+        public GeneralResponse(bool succeeded, object data, string message)
         {
             Succeeded = succeeded;
             Data = data;
@@ -30,18 +28,18 @@ namespace TaghcheBookInfo.Application.Common
             return new(true);
         }
 
-        public static GeneralResponse Failed(GeneralErrors error = GeneralErrors.InternalServerError)
+        public static GeneralResponse Failed()
         {
-            return new(false, error);
+            return new(false);
         }
 
         public static GeneralResponse Success(object data, string message)
         { 
             return new(true, data, message);
         }
-        public static GeneralResponse Failed(string message, GeneralErrors error = GeneralErrors.InternalServerError)
+        public static GeneralResponse Failed(string message)
         {
-            return new(false, null, message, error);
+            return new(false, null, message);
         }
 
         public static implicit operator bool(GeneralResponse managerResult)
